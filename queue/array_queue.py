a74c0e87a802023adc4d17423abad6c929b35683
Song Hui <ifournight@gmail.com>
2020-11-20 12:06:55 +0800

    Better code array queue

--- queue/array_queue.py
+++ queue/array_queue.py
@@ -1,5 +1,5 @@
-# Queue implemented with array, whose time complexity of enqueue is O(1)
-# and amotized time complexity of dequeue is O(1)
+# Queue implemented with array, whose amotized time complexity of enqueue is O(1)
+# and time complexity of dequeue is O(1)
 class ArrayQueue(object):
   def __init__(self, capacity):
     self.array = []
@@ -12,32 +12,29 @@ class ArrayQueue(object):
 
   def __str__(self):
     return str(self.array)
-    # return str(self.array[self.head:self.tail])
 
   def enqueue(self, item):
     if self.tail == self.capacity:
       if self.head == 0:
         print('Queue is full, enqueue failed.')
-        return
+        return False
       else:
         self.array = self.array[self.head:self.tail]
         self.tail = self.tail - self.head
         self.head = 0
     self.array.append(item)
     self.tail += 1
+    return True
   
   def dequeue(self):
-    if self.tail == self.head:
+    if self.tail != self.head:
+      item = self.array[self.head]
+      self.array[self.head] = None
+      self.head += 1
+      return item
+    else:
       print('Queue is Empty, dequeue failed.')
       return None
-    item = self.array[self.head]
-    self.array[self.head] = None
-    self.head += 1
-    if self.tail == self.head:
-      del self.array[:self.head]
-      self.head = 0
-      self.tail = 0
-    return item
 
 if __name__ == "__main__":
   queue = ArrayQueue(10)
@@ -45,7 +42,7 @@ if __name__ == "__main__":
     queue.enqueue(i)
     print(queue)
   queue.enqueue(11)
-  for i in range(5):
+  for i in range(20):
     print('dequeue: %s' % queue.dequeue())
     print('%s, head: %s, tail: %s' % (str(queue), queue.head, queue.tail))
   for i in range(10, 20):
